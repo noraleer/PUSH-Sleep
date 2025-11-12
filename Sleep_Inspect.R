b6d07f26e1a57723fb48d7eb5b6a05b37f921920
Oliver Schramm <schrammwoliver12@gmail.com>
2025-11-12 12:34:23 -0600

    Pathways to open all files

---
+++ Sleep_Inspect.R
@@ -0,0 +1,9 @@
+Empathy <- readxl::read_xlsx("Eeep/Empathy_Data_9.25.2025_HLM Analyses.xlsx")
+
+actigraphy_t1 <- readxl::read_xlsx("Eeep/Esleep_T1_Sleep Data_DailyActi_4.22.25.xlsx")
+
+actigraphy_t2 <- readxl::read_xlsx("Eeep/Esleep_T2_Sleep Data_DailyActi_4.22.25.xlsx")
+
+diary_t2 <- readxl::read_xlsx("Eeep/E-Sleep_DailyDiary_FINAL_T2Data_5.22.2025.xlsx")
+
+diary_t1 <- readxl::read_xlsx("Eeep/E-Sleep_DailyDiary_FINAL_T1Data_5.22.2025.xlsb.xlsx")
