11abaa6b42b8b904fe50f2817e4b304ade168eed
May Norman <maynorman@Mays-MacBook-Air.local>
2025-11-12 22:31:42 -0600

    Fixed Problem with dates

--- Sleep_Inspect.R
+++ Sleep_Inspect.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 
-Empathy <- readxl::read_xlsx("../Eeep/Empathy_Data_9.25.2025_HLM Analyses.xlsx")
+Empathy <- readxl::read_xlsx("./Eeep/Empathy_Data_9.25.2025_HLM Analyses.xlsx")
 
-actigraphy_t1 <- readxl::read_xlsx("../Eeep/Esleep_T1_Sleep Data_DailyActi_4.22.25.xlsx")
+actigraphy_t1 <- readxl::read_xlsx("./Eeep/Esleep_T1_Sleep Data_DailyActi_4.22.25.xlsx")
 
-actigraphy_t2 <- readxl::read_xlsx("../Eeep/Esleep_T2_Sleep Data_DailyActi_4.22.25.xlsx")
+actigraphy_t2 <- readxl::read_xlsx("./Eeep/Esleep_T2_Sleep Data_DailyActi_4.22.25.xlsx")
 
-diary_t2 <- readxl::read_xlsx("../Eeep/E-Sleep_DailyDiary_FINAL_T2Data_5.22.2025.xlsx")
+diary_t2 <- readxl::read_xlsx("./Eeep/E-Sleep_DailyDiary_FINAL_T2Data_5.22.2025.xlsx")
 
-diary_t1 <- readxl::read_xlsx("../Eeep/E-Sleep_DailyDiary_FINAL_T1Data_5.22.2025.xlsb.xlsx")
+diary_t1 <- readxl::read_xlsx("./Eeep/E-Sleep_DailyDiary_FINAL_T1Data_5.22.2025.xlsb.xlsx")
 
 ## Going to have to rename subject_id 
 
@@ -18,3 +18,4 @@ actigraphy_t1_dec <- actigraphy_t1 %>%
 actigraphy_t2_dec <- actigraphy_t2 %>% 
   select(-start_time_t2, -end_time_t2)
 
+diary_t1$T1_DD_NightDate = as.Date(diary_t1$T1_DD_NightDate, origin = "1899-12-30")
