ffdc3f5b284d33cfe24653472d5d3de830277bc0
Oliver Schramm <schrammwoliver12@gmail.com>
2025-11-14 15:28:45 -0600

    Bindrows is almost ready to be run, but we need column types to match up. Could be problematic when we have time ranges put into one column. (Example, T_DD_NapOnset).

--- Period_Pain/Code/Sleep_Inspect.R
+++ Period_Pain/Code/Sleep_Inspect.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 
-Empathy <- readxl::read_xlsx("./Eeep/Empathy_Data_9.25.2025_HLM Analyses.xlsx")
+Empathy <- readxl::read_xlsx("./Period_Pain/Data/Empathy_Data_9.25.2025_HLM Analyses.xlsx")
 
-actigraphy_t1 <- readxl::read_xlsx("./Eeep/Esleep_T1_Sleep Data_DailyActi_4.22.25.xlsx")
+actigraphy_t1 <- readxl::read_xlsx("./Period_Pain/Data/Esleep_T1_Sleep Data_DailyActi_4.22.25.xlsx")
 
-actigraphy_t2 <- readxl::read_xlsx("./Eeep/Esleep_T2_Sleep Data_DailyActi_4.22.25.xlsx")
+actigraphy_t2 <- readxl::read_xlsx("./Period_Pain/Data/Esleep_T2_Sleep Data_DailyActi_4.22.25.xlsx")
 
-diary_t2 <- readxl::read_xlsx("./Eeep/E-Sleep_DailyDiary_FINAL_T2Data_5.22.2025.xlsx")
+diary_t2 <- readxl::read_xlsx("./Period_Pain/Data/E-Sleep_DailyDiary_FINAL_T2Data_5.22.2025.xlsx")
 
-diary_t1 <- readxl::read_xlsx("./Eeep/E-Sleep_DailyDiary_FINAL_T1Data_5.22.2025.xlsb.xlsx")
+diary_t1 <- readxl::read_xlsx("./Period_Pain/Data/E-Sleep_DailyDiary_FINAL_T1Data_5.22.2025.xlsb.xlsx")
 
 ## Going to have to rename subject_id 
 
@@ -33,4 +33,33 @@ t1_joined <- diary_t1 %>%
 t2_joined <- diary_t2 %>% 
   full_join(actigraphy_t2_dec, by = c("C_ID" = "subject_id_t2", "T2_DD__DD_Interval" = "interval_number_t2"))
 
-### Yadaydayda
+names(t1_joined) <- gsub("T1", "T", names(t1_joined))
+names(t1_joined) <- gsub("t1", "T", names(t1_joined))
+t1_joined
+names(t2_joined) <- gsub("T2", "T", names(t2_joined))
+names(t2_joined) <- gsub("t2", "T", names(t2_joined))
+t2_joined
+
+setdiff(names(t1_joined), names(t2_joined))
+setdiff(names(t2_joined), names(t1_joined))
+
+t2 <- t2_joined %>%
+  rename(T_DD_DD_Interval = T_DD__DD_Interval,
+         T_DD_NapDuration = "T_DD_-NapDuration",
+         T_DD_CaffType_Juice = T_DD_CaffType_Juices,
+         T_DD_MedType3_SSRIs = T_DD_Medtype3_SSRIs,
+         onset_latency_T = onset_latency_T...9,
+         sleep_midpoint_T = "sleep midpoint_T") %>% 
+  select(-onset_latency_T...10)
+
+t1 <- t1_joined %>% 
+  rename(T_DD_CaffAmt_LessThanTheRecommendedAmt = T_DD_CaffAmt_LessThanTheRecommendedAmount,
+         T_DD_CaffAmt_TheRecommendedAmt = T_DD_CaffAmt_TheRecommendedAmount,
+         T_DD_CaffAmt_GreaterThanTheRecommendedAmt = T_DD_CaffAmt_GreaterThanTheRecommendedAmount)
+
+setdiff(names(t2), names(t1))
+setdiff(names(t1), names(t2)) # Everything in t1 is present in t2, but MedType6 is not present in t1. 
+
+t1 %>% 
+  mutate(T_DD_NapOnset = as.double(T_DD_NapOnset)) %>% 
+  view()
